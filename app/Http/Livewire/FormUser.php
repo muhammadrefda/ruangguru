8c59878a1ddc9c2bb9a169819e579cb81710adb9
refda <muhammadrefda@gmail.com>
2021-07-10 22:04:37 +0700

    Create Form page

---
+++ app/Http/Livewire/FormUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class FormUser extends Component
+{
+    public function render()
+    {
+        return view('livewire.form-user');
+    }
+}
---
+++ resources/views/livewire/form-user.blade.php
@@ -0,0 +1,54 @@
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <div>
+        <x-jet-authentication-card-logo />
+    </div>
+
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <form method="POST" action="">
+            @csrf
+            <div class="mt-4">
+                <x-jet-label for="userName" value="{{ __('Fullname') }}" />
+                <x-jet-input id="userName" class="block mt-1 w-full" type="text" name="userName" :value="old('userName')" required autofocus />
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="userPhoneNumber" value="{{ __('Contact Number') }}" />
+                <x-jet-input id="userPhoneNumber" class="block mt-1 w-full" type="number" name="userPhoneNumber" required />
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="userPhoneNumber" value="{{ __('Street Address') }}" />
+                <x-jet-input id="userPhoneNumber" class="block mt-1 w-full" type="text" name="userPhoneNumber" required />
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="village" value="{{ __('Village') }}" />
+                <x-jet-input id="village" class="block mt-1 w-full" type="text" name="village" required />
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="subDistrict" value="{{ __('Sub-District') }}" />
+                <x-jet-input id="subDistrict" class="block mt-1 w-full" type="text" name="subDistrict" required />
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="district" value="{{ __('District') }}" />
+                <x-jet-input id="district" class="block mt-1 w-full" type="text" name="district" required />
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="city" value="{{ __('City') }}" />
+                <x-jet-input id="city" class="block mt-1 w-full" type="text" name="city" required />
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="province" value="{{ __('Province') }}" />
+                <x-jet-input id="province" class="block mt-1 w-full" type="text" name="province" required />
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="postalCode" value="{{ __('zip code / postal code') }}" />
+                <x-jet-input id="postalCode" class="block mt-1 w-full" type="text" name="postalCode" required />
+            </div>
+            <x-jet-button class="mt-4">
+                {{ __('Submit') }}
+            </x-jet-button>
+        </form>
+    </div>
+</div>
