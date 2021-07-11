ed2eca6a4225e01b6c2ab9e431ac853c8d3d3916
refda <muhammadrefda@gmail.com>
2021-07-11 14:32:53 +0700

    change the url for form-user page

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::get('form-user', FormUser::class);
+Route::get('/', FormUser::class);
