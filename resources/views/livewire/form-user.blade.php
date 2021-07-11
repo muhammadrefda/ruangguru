ea27191d2f4e40b274e7735a2d2690ebdf5a0514
refda <muhammadrefda@gmail.com>
2021-07-11 14:15:07 +0700

    Add product subscription

--- resources/views/livewire/form-user.blade.php
+++ resources/views/livewire/form-user.blade.php
@@ -1,11 +1,26 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
+    <div class="mt-4">
         <x-jet-authentication-card-logo />
     </div>
 
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         <form method="POST" action="">
             @csrf
+            <div class="mt-4">
+                <x-jet-label for="" value="{{ __('Product Subscription') }}" />
+                <div>
+                <x-jet-input id="userName" class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" type="radio" name="userName" required autofocus />
+                    English Academy
+                </div>
+                <div>
+                    <x-jet-input id="userName" class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" type="radio" name="userName" required autofocus />
+                    Skill Academy
+                </div>
+                <div>
+                    <x-jet-input id="userName" class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" type="radio" name="userName" required autofocus />
+                    Ruangguru
+                </div>
+            </div>
             <div class="mt-4">
                 <x-jet-label for="userName" value="{{ __('Fullname') }}" />
                 <x-jet-input id="userName" class="block mt-1 w-full" type="text" name="userName" :value="old('userName')" required autofocus />
